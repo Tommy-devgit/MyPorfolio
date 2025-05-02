e96d0d6507220d75b9851e6fbb80f5b28974e7e3
Tommy <tomasmelesse413@gmail.com>
2025-05-02 13:47:06 +0300

    some changes on index.html and css

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 document.getElementById("year").textContent = new Date().getFullYear();
-
-
-// Select all the section links
 const navLinks = document.querySelectorAll('.navbar h1 a');
 const sections = document.querySelectorAll('.section');
-
-// Add an event listener to handle scroll
 window.addEventListener('scroll', () => {
   let currentSection = "";
-
-  // Loop through sections to find which one is currently in view
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
@@ -18,7 +11,6 @@ window.addEventListener('scroll', () => {
     }
   });
 
-  // Add active class to the corresponding nav link
   navLinks.forEach((link) => {
     link.classList.remove('active');
     if (link.getAttribute('href') === `#${currentSection}`) {
